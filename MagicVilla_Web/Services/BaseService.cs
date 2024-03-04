c4877df48eb45d49c0286b35943ad11b587f618c
Alexandru Ologu <ologu.alexandru@gmail.com>
2024-03-04 12:59:37 +0200

    Base Service

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,35 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MagicVilla_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+
+        public IHttpClientFactory httpClient {  get; set; }
+
+        public BaseService(IHttpClientFactory httpClient) 
+        {
+            this.responseModel = new();
+            this.httpClient = httpClient;
+        }
+
+        public Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MagicAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");  
+                }
+            }
+        }
+    }
+}
